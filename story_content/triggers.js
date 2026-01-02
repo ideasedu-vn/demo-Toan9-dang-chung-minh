df41f63a33e9ecb761cc3025c93b7280d504fc5e
ideasedu-vn <115155233+ideasedu-vn@users.noreply.github.com>
2026-01-02 15:18:34 +0700

    up lại

--- story_content/triggers.js
+++ story_content/triggers.js
@@ -2,58 +2,58 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5m2MVMGNSma":
+      case "6oAsYfanRjh":
         Script1();
         break;
-      case "61eWFwqHA8z":
+      case "64pedYp89HA":
         Script2();
         break;
-      case "6SiaTuzOOri":
+      case "6e6sYJa4Yll":
         Script3();
         break;
-      case "5woOAy6Dnh7":
+      case "6lygU146I3l":
         Script4();
         break;
-      case "5YgGBjYi7SB":
+      case "5e2zBJq8cLr":
         Script5();
         break;
-      case "6WLNeAkWqxp":
+      case "6TRBCeivFVr":
         Script6();
         break;
-      case "6AmBQmLDUWC":
+      case "5iMIVX6vYy8":
         Script7();
         break;
-      case "6A5KI2s8Ncw":
+      case "5qdYt7iSzPt":
         Script8();
         break;
-      case "6ZHDByRJmr2":
+      case "5cEBmDwkIiA":
         Script9();
         break;
-      case "6NagXFiMYGv":
+      case "69cFAA9vTSo":
         Script10();
         break;
-      case "6TSHsauW5zQ":
+      case "6PQjzTPiW6p":
         Script11();
         break;
-      case "6c4k8gx5kpB":
+      case "6Ufu1ydejk2":
         Script12();
         break;
-      case "69nP1f8Wo6t":
+      case "6iqg1YGX4FN":
         Script13();
         break;
-      case "6BRnXztZtjp":
+      case "6H4rcjkBEEi":
         Script14();
         break;
-      case "63TfJKas1YH":
+      case "6IZ4ZB5VTe2":
         Script15();
         break;
-      case "6RpG6Q26ylv":
+      case "6WsyBdUVvbu":
         Script16();
         break;
-      case "63qPVeyIjLP":
+      case "5bXCdiX7oxB":
         Script17();
         break;
-      case "5xeudZLXX7k":
+      case "5e2ljdsjtP4":
         Script18();
         break;
   }
